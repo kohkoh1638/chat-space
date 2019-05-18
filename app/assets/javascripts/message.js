8c99376084a51276ae70096fba540786fdc2f95f
kohkoh1638 <kohkoh1638@yahoo.co.jp>
2019-05-18 20:13:35 +0900

    var html-done-add

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,12 +1,42 @@
-$('#new_message').on('submit', function(e){
-  e.preventDefault();
-  var formData = new FormData(this);
-  var url = $(this).attr('action')
-  $.ajax({
-    url: url,
-    type: "POST",
-    data: formData,
-    dataType: 'json',
-    processData: false,
-    contentType: false,
-  })
+$(function(){
+  function buildHTML(message){
+    image = ( message.image ) ? `<img class= "lower-message__image" src=${message.image} >` : "";
+    var html =
+    `<div class="message" data-message-id= "${message.id}">
+      <div class="upper-message">
+        <div class="upper-message__user-name">
+          ${message.user_name}
+        </div>
+        <div class="upper-message__date">
+          ${message.date}
+        </div>
+      </div>
+      <div class="lower-meesage">
+        <p class="lower-message__content">
+          ${message.content}
+        </p>
+      </div>
+      ${image}
+    </div>`
+
+    return html;
+  }
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false,
+    })
+    .done(function(data){
+		  var html = buildHTML(data);
+	  	$('.messages').append(html);
+	  .fail(function(){
+	    alert('error');
+	  });
+})
