7ae8d9b26b3d81ae9a3a188224f55af39513d24c
kohkoh1638 <kohkoh1638@yahoo.co.jp>
2019-05-18 21:19:35 +0900

    ScrollToNewMessage-add-message.date-fix

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -8,7 +8,7 @@ $(function(){
           ${message.user_name}
         </div>
         <div class="upper-message__date">
-          ${message.date}
+          ${message.created_at}
         </div>
       </div>
       <div class="lower-meesage">
@@ -21,22 +21,32 @@ $(function(){
 
     return html;
   }
+
+  function ScrollToNewMessage(){
+    $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+  }
+
   $('#new_message').on('submit', function(e){
-    e.preventDefault();
-    var formData = new FormData(this);
-    var url = $(this).attr('action')
-    $.ajax({
-      url: url,
-      type: "POST",
-      data: formData,
-      dataType: 'json',
-      processData: false,
-      contentType: false,
-    })
-    .done(function(data){
-		  var html = buildHTML(data);
-	  	$('.messages').append(html);
-	  .fail(function(){
-	    alert('error');
-	  });
-})
+      e.preventDefault();
+      var formData = new FormData(this);
+      var url = $(this).attr('action')
+      $.ajax({
+        url: url,
+        type: "POST",
+        data: formData,
+        dataType: 'json',
+        processData: false,
+        contentType: false,
+      })
+      .done(function(data){
+        var html = buildHTML(data);
+        $('.messages').append(html);
+        ScrollToNewMessage();
+        $('.form__message').val('');
+        $(".form__submit").prop('disabled', false);
+      })
+      .fail(function(){
+        alert('error');
+       })
+   })
+});
