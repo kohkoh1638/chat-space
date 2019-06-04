04fb09dcf62ca5885aabe11cac4db9f7096bc7d1
kohkoh1638 <kohkoh1638@yahoo.co.jp>
2019-06-05 07:50:00 +0900

    production.rb-add

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,1 +1,8 @@
 server '54.65.199.203', user: 'ec2-user', roles: %w{app db web}
+
+set :ssh_options, {
+  port: 22,　#XXXにポート番号
+  keys: [File.expand_path('~/.ssh/id_rsa')],　#'~/.ssh/id_rsa'部分をローカル環境の鍵のパスに
+  forward_agent: true,
+  auth_methods: %w(publickey)
+}
